1b0284ddd335bd4cf74be6e35f0b2c89e552ffc3
Luke Gessler <lukegessler@gmail.com>
2025-10-24 11:37:40 -0400

    fix #1

--- src/onf_parser/__init__.py
+++ src/onf_parser/__init__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib.metadata import version, PackageNotFoundError
 
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+    __version__ = version(dist_name)
+except PackageNotFoundError:
     __version__ = "unknown"
 finally:
-    del get_distribution, DistributionNotFound
+    del version, PackageNotFoundError
 
 from onf_parser.parse import parse_file, parse_files, parse_file_string
 from onf_parser.models import (
